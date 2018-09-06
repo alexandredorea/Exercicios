b5e798826cc705640d632613695a4e7e61efd22d
Alexandre Dórea <alexandre.dorea@hbsis.com.br>
2018-09-06 13:08:39 -0300

    Commit Exercício 05 do módulo de estrutura de condições

--- Lógica de Programação/02. Comandos Seleção/Algoritmo.Selecao.Exercicio05/Program.cs
+++ Lógica de Programação/02. Comandos Seleção/Algoritmo.Selecao.Exercicio05/Program.cs
@@ -6,7 +6,31 @@ namespace Algoritmo.Selecao.Exercicio05
     {
         private static void Main()
         {
-            Console.Write("");
+            Console.Write("Informe o tamanho do primeiro lado do triângulo: ");
+	        var ladoX = Convert.ToInt32(Console.ReadLine());
+			Console.Clear();
+
+	        Console.Write("Informe o tamanho do segundo lado do triângulo: ");
+	        var ladoY = Convert.ToInt32(Console.ReadLine());
+	        Console.Clear();
+
+			Console.Write("Informe o tamanho do terceiro lado do triângulo: ");
+	        var ladoZ = Convert.ToInt32(Console.ReadLine());
+	        Console.Clear();
+
+			if (ladoX > (ladoY + ladoZ) || ladoY > (ladoX + ladoZ) || ladoZ > (ladoY + ladoX))
+				Console.WriteLine("Não é um triângulo");
+	        else
+	        {
+		        if (ladoX == ladoY && ladoX == ladoZ)
+			        Console.WriteLine("Este é um triângulo equilatero"); 
+				else if (ladoX != ladoY && ladoX != ladoZ && ladoY != ladoZ)
+			        Console.WriteLine("Este é um triângulo escaleno");
+				else 
+			        Console.WriteLine("Este é um triângulo isósceles");
+			}
+
+	        Console.ReadKey();
         }
     }
 }
